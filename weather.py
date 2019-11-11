661e05be3b09904e526df01f9c18bb7376773ed9
Aron <aron.pap8@gmail.com>
2019-11-11 09:18:58 +0100

    Made the 'city' as an optinal parameter for the API call

--- weather.py
+++ weather.py
@@ -5,7 +5,9 @@ import requests
 import json
 import pymongo
 
-url = 'http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=bd64898a234d3ac401ec97cfd4ba3f2d'
+#url = 'http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=bd64898a234d3ac401ec97cfd4ba3f2d'
+city = 'Budapest'
+url = 'http://api.openweathermap.org/data/2.5/weather?q={}&APPID=bd64898a234d3ac401ec97cfd4ba3f2d'.format(city)
 
 res = requests.get(url)
 temp = json.loads(res.content)
