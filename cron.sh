14a50915a42b78f85193871783e70a7e44876e37
Ubuntu <ubuntu@ip-172-31-20-58.us-east-2.compute.internal>
2019-11-22 15:56:17 +0000

    updated credentials

--- cron.sh
+++ cron.sh
@@ -1,4 +1,3 @@
-/usr/bin/docker login --username 18435 --password Pap8Dok!tor &&
 /usr/bin/docker run --name mongotest -t --net host 18435/awesome &&
 /usr/bin/docker stop mongotest &&
 /usr/bin/docker rm mongotest &&
